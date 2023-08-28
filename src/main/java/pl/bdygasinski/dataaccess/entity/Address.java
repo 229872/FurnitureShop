e7c0bed8f1c35c0c260882690b588129e277a7b9
Bartłomiej Dygasiński <bartek.dygasinski00@gmail.com>
2023-08-28 12:29:29 +0200

    Create PersonalData, Address

---
+++ src/main/java/pl/bdygasinski/dataaccess/entity/Address.java
@@ -0,0 +1,30 @@
+package pl.bdygasinski.dataaccess.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+
+@Entity
+public class Address extends AbstractEntity {
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String street;
+
+    @Column(nullable = false)
+    private Integer streetNumber;
+
+    @Column(nullable = false)
+    private String postalCode;
+}
--- src/main/java/pl/bdygasinski/dataaccess/entity/PersonalData.java
+++ src/main/java/pl/bdygasinski/dataaccess/entity/PersonalData.java
@@ -1,7 +1,9 @@
 package pl.bdygasinski.dataaccess.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDate;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
@@ -11,4 +13,17 @@ import lombok.*;
 
 @Entity
 public class PersonalData extends AbstractEntity {
+
+    @Column(nullable = false, name = "first_name")
+    private String firstName;
+
+    @Column(nullable = false, name = "last_name")
+    private String lastName;
+
+    @Column(nullable = false, name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(nullable = false, updatable = false, unique = true)
+    private Address address;
 }
