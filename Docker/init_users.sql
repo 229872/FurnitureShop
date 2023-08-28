1eb9780f80ba5a3c8363321a9c7db293166ca1d4
Bartłomiej Dygasiński <bartek.dygasinski00@gmail.com>
2023-08-28 20:44:52 +0200

    Change sql script

--- Docker/init_users.sql
+++ Docker/init_users.sql
@@ -1,2 +1,12 @@
-CREATE USER shop_creator WITH PASSWORD '${SHOP_CREATOR_PASSWORD}';
-CREATE USER shop_account_manager WITH PASSWORD '${SHOP_ACCOUNT_MANAGER_PASSWORD}';
+-- Script used for local environment, create production environment with secret passwords instead
+-- Set passwords as environment variables.
+CREATE USER shop_creator WITH PASSWORD 'dbcreator';
+CREATE USER shop_account_manager WITH PASSWORD 'dbaccountmanager';
+
+REVOKE ALL ON DATABASE furniture_shop FROM PUBLIC;
+REVOKE ALL ON DATABASE postgres FROM PUBLIC;
+GRANT CONNECT ON DATABASE furniture_shop TO shop_creator, shop_account_manager;
+
+REVOKE ALL ON SCHEMA public FROM public;
+GRANT USAGE ON SCHEMA public TO shop_creator, shop_account_manager;
+GRANT CREATE ON SCHEMA public TO shop_creator;
