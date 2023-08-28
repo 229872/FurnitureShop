a5361315dfac4d54e023dd36dcdc6f86dba3930c
Bartłomiej Dygasiński <bartek.dygasinski00@gmail.com>
2023-08-28 13:13:11 +0200

    Change datasource, persistence.xml, new user, rm .idea

--- src/main/java/pl/bdygasinski/config/DataSourceConfig.java
+++ src/main/java/pl/bdygasinski/config/DataSourceConfig.java
@@ -10,6 +10,8 @@ import java.sql.Connection;
 
 @ApplicationScoped
 @DataSourceDefinitions({
+
+        //You have to set environment variables in your OS or in application server to have access to database.
         @DataSourceDefinition(
                 name = "java:app/init",
                 className = "org.postgresql.ds.PGSimpleDataSource",
@@ -17,13 +19,27 @@ import java.sql.Connection;
                 portNumber = 5555,
                 databaseName = "furniture_shop",
                 user = "shop_creator",
-                //You have to set environment variable in your OS or in application server.
-                password = "${DB_PASSWORD}",
+                password = "${SHOP_CREATOR_PASSWORD}",
                 isolationLevel = Connection.TRANSACTION_READ_COMMITTED,
-                initialPoolSize = 4,
+                initialPoolSize = 1,
+                minPoolSize = 1,
+                maxPoolSize = 1,
+                description = "Data source for creating tables."
+        ),
+        @DataSourceDefinition(
+                name = "java:app/account",
+                className = "org.postgresql.ds.PGSimpleDataSource",
+                serverName = "localhost",
+                portNumber = 5555,
+                databaseName = "furniture_shop",
+                user = "shop_account_manager",
+                password = "${SHOP_ACCOUNT_MANAGER_PASSWORD}",
+                isolationLevel = Connection.TRANSACTION_READ_COMMITTED,
+                initialPoolSize = 3,
                 minPoolSize = 2,
-                maxPoolSize = 6,
-                description = "Data source for creating tables"
+                maxPoolSize = 5,
+                description = "Data source for account functionality."
+
         )
 })
 public class DataSourceConfig {
